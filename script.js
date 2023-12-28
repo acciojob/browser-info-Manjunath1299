903b5d97e8ac8fcaffeef16230dafb7f3296ea76
Manjunath <66263712+Manjunath1299@users.noreply.github.com>
2023-12-28 19:33:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
-//your JS code here. If required.
-document.addEventListener('DOMContentLoaded', function () {
-    var browserInfoDiv = document.getElementById('browser-info');
+function displayBrowserInfo() {
+  // Get the user agent string
+  const userAgent = navigator.userAgent;
 
-    // Get browser information from the navigator object
-    var browserName = navigator.appName;
-    var browserVersion = navigator.appVersion;
+  // Extract the browser name and version
+  let browserName;
+  let version = "";
 
-    // Display the information in the div
-    browserInfoDiv.textContent = "You are using " + browserName + " version " + browserVersion;
-});
+  if (userAgent.includes("Firefox")) {
+    browserName = "Mozilla Firefox";
+  } else if (userAgent.includes("Chrome")) {
+    browserName = "Google Chrome";
+  } else if (userAgent.includes("Safari")) {
+    browserName = "Apple Safari";
+  } else if (userAgent.includes("Edge")) {
+    browserName = "Microsoft Edge";
+  } else if (userAgent.includes("Opera")) {
+    browserName = "Opera";
+  } else {
+    browserName = "Unknown Browser";
+  }
+
+  // Extract version number if available
+  if (browserName !== "Unknown Browser") {
+    const versionMatch = userAgent.match(/(Chrome|Firefox|Safari|Edge|Opera)\/(\d+(\.\d+)?)/);
+    if (versionMatch) {
+      version = versionMatch[2];
+    }
+  }
+
+  // Create the message
+  const message = "You are using " + browserName + " version " + version;
+
+  // Get the div element to display the message
+  const browserInfoDiv = document.getElementById("browser-info");
+
+  // Display the message
+  browserInfoDiv.textContent = message;
+}
+
+// Call the function to display the information immediately
+displayBrowserInfo();
